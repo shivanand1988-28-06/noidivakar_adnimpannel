0afe652dbbdddd2928328a5385460c5de8198e16
YourGitHubUsername <your.email@example.com>
2025-12-24 19:33:38 -0800

    Fix Prettier: add space in assignedTasks state declaration

--- material-dashboard-react-main/src/layouts/dashboard/index.js
+++ material-dashboard-react-main/src/layouts/dashboard/index.js
@@ -51,7 +51,7 @@ function Dashboard() {
       if (adminUser === "Admin") {
         setLoading(true);
         setCurrentUser(adminUser);
-        console.log(currentUser);
+        console.log(currentUser,adminUser);
         try {
           const response = await fetch(`${API_BASE}/api/admin/all-names`, {
             method: "GET",
